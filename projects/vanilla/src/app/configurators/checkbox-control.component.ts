8aafaac66e8f19e3f20b7c5c8ae327667660d522
Leibenguth <leibenguth@sinequa.com>
2022-02-14 11:28:43 +0100

    Added model changed event on the checkbox

--- projects/vanilla/src/app/configurators/checkbox-control.component.ts
+++ projects/vanilla/src/app/configurators/checkbox-control.component.ts
@@ -1,11 +1,11 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, Output, EventEmitter } from "@angular/core";
 import { ConfiguratorContext } from "ngx-ui-builder";
 
 @Component({
     selector: 'sq-checkbox',
     template: `
 <div class="form-check mb-2">
-    <input class="form-check-input" type="checkbox" [id]="property" [(ngModel)]="!!context.config[property]" (ngModelChange)="context.configChanged()">
+    <input class="form-check-input" type="checkbox" [id]="property" [(ngModel)]="!!context.config[property]" (ngModelChange)="onModelChanged($event)">
     <label class="form-check-label" [for]="property">{{label || property}}</label>
 </div>
     `
@@ -14,4 +14,11 @@ export class CheckboxControlComponent {
     @Input() context: ConfiguratorContext;
     @Input() property: string;
     @Input() label?: string;
+
+    @Output() modelChanged = new EventEmitter<boolean>();
+
+    onModelChanged(value: boolean) {
+        this.context.configChanged();
+        this.modelChanged.next(value);
+    }
 }
