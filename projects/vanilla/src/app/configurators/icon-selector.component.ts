db15416d81056d9621b0eddda8a69e1892399534
Leibenguth <leibenguth@sinequa.com>
2022-03-23 15:10:16 +0100

    Refactored icon selector to use new autocomplete

--- projects/vanilla/src/app/configurators/icon-selector.component.ts
+++ projects/vanilla/src/app/configurators/icon-selector.component.ts
@@ -1,5 +1,6 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, EventEmitter, Input, Output } from "@angular/core";
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from "@angular/core";
 import { ComponentConfig } from "ngx-ui-builder";
+import { Observable, of } from "rxjs";
 import faIcons from './fontawesomeicons.json';
 
 declare interface ComponentWithIconConfig extends ComponentConfig {
@@ -11,75 +12,42 @@ declare interface ComponentWithIconConfig extends ComponentConfig {
     template: `
 <label for="icon">Icon <span *ngIf="config.icon">(<i [ngClass]="config.icon"></i>)</span></label>
 <input type="text"
-    class="form-control mb-2"
+    #inputElement
+    class="form-control"
     id="icon"
     autocomplete="off"
     spellcheck="off"
     [(ngModel)]="config.icon"
-    (ngModelChange)="iconChanged()"
-    (focus)="onFocus()"
-    (blur)="onBlur()">
-<div class="position-relative" *ngIf="suggests?.length">
-    <div class="card list-group list-group-flush position-absolute">
-        <div *ngFor="let suggest of suggests" class="list-group-item-action px-2 py-1" (click)="select(suggest)">
-            <i class="fa-fw" [ngClass]="suggest"></i> {{suggest}}
-        </div>
-    </div>
-</div>
+    (ngModelChange)="iconChanged()">
+<uib-autocomplete [inputElement]="inputElement" [suggestGenerator]="suggestGenerator" (select)="select($event)">
+    <ng-template #itemTpl let-suggest><i class="fa-fw" [ngClass]="suggest"></i> {{suggest}}</ng-template>
+</uib-autocomplete>
     `,
-    styles: [`
-.list-group {
-    top: 0;
-    width: 100%;
-    max-height: 200px;
-    overflow: auto;
-}
-    `],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class IconSelectorComponent {
     @Input() config: ComponentWithIconConfig;
     @Output() configChanged = new EventEmitter();
-
-    suggests?: string[];
-
-    constructor(
-        public cdref: ChangeDetectorRef
-    ){}
-
-    onFocus() {
-        this.updateSuggests();
-    }
-
-    onBlur() {
-        setTimeout(() => { // Set timeout is necessary because onBlur() is called before select() upon a click
-            this.suggests = undefined;
-            this.cdref.markForCheck();
-        });
-    }
-
+    
     iconChanged() {
-        this.updateSuggests();
         this.configChanged.next();
-    }
-
-    updateSuggests() {
-        const value = this.config.icon || '';
-        const faPattern = /\bfa-([\w-]+)/g;
-        const match = faPattern.exec(value);
-        const token = match?.[1];
-        if(token) {
-            const icons = faIcons as string[];
-            this.suggests = icons.filter(i => i.startsWith(token.toLowerCase()))
-                .map(i => value.replace(faPattern, "fa-"+i))
-        }
-        else {
-            this.suggests = undefined;
-        }
     }
 
     select(suggest: string) {
         this.config.icon = suggest;
-        this.configChanged.next();
+        this.iconChanged();
+    }
+
+    suggestGenerator = (input: string): Observable<string[]> => {
+        const faPattern = /\bfa-([\w-]+)/g;
+        const match = faPattern.exec(input);
+        const token = match?.[1];
+        let suggests: string[] = [];
+        if(token) {
+            const icons = faIcons as string[];
+            suggests = icons.filter(i => i.startsWith(token.toLowerCase()))
+                .map(i => input.replace(faPattern, "fa-"+i))
+        }
+        return of(suggests);
     }
 }
