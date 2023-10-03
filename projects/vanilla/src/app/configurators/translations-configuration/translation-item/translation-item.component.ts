9ce934641f8873d78312f98b400af4ac7273abab
LEIBFRIED Guillaume <guillaume.leibfried@sinequa.com>
2023-10-03 16:11:18 +0200

    make add node and key two different buttons

--- projects/vanilla/src/app/configurators/translations-configuration/translation-item/translation-item.component.ts
+++ projects/vanilla/src/app/configurators/translations-configuration/translation-item/translation-item.component.ts
@@ -58,69 +58,59 @@ export class TranslationItemComponent implements OnChanges {
     });
   }
 
-  addItem() {
-    this.modalService.confirm({
-      confirmType: ConfirmType.Info,
-      title: "Item type",
-      message: "Do you want to add a translation key or a node?",
-      buttons: [
-        new ModalButton({result: ModalResult.Yes, text: 'Key'}),
-        new ModalButton({result: ModalResult.No, text: 'Node'}),
-      ]
-    }).then(res => {
-      if (res === ModalResult.Yes) { // translation
-        let model: PromptOptions = {
-          message: 'Translation key',
-          buttons: [],
-          output: '',
-          validators: [Validators.required]
-        };
+  addKey() {
+    let model: PromptOptions = {
+      message: 'Translation key',
+      buttons: [],
+      output: '',
+      validators: [Validators.required]
+    };
 
-        // Fetch the item key
-        this.modalService.prompt(model).then(res => {
-          if (res === ModalResult.OK) {
-            const key = model.output;
-            model.output = '';
-            model.message = 'Translation value';
+    // Fetch the item key
+    this.modalService.prompt(model).then(res => {
+      if (res === ModalResult.OK) {
+        const key = model.output;
+        model.output = '';
+        model.message = 'Translation value';
 
-            // Fetch the item value
-            this.modalService.prompt(model).then(res => {
-              if (res === ModalResult.OK) {
-                const item = {};
-                item[key] = model.output;
-
-                this.item.value = {
-                  ...this.item.value,
-                  ...item
-                }
-
-                this.updated.emit({ path: this.currentPath, value: this.item.value });
-              }
-            });
-          }
-        });
-      } else if (res === ModalResult.No) { // node
-        const model: PromptOptions = {
-          message: 'Node key',
-          buttons: [],
-          output: '',
-          validators: [Validators.required]
-        };
-
-        // Fetch the node key
+        // Fetch the item value
         this.modalService.prompt(model).then(res => {
           if (res === ModalResult.OK) {
             const item = {};
-            item[model.output] = {};
+            item[key] = model.output;
 
             this.item.value = {
               ...this.item.value,
               ...item
-            };
+            }
 
             this.updated.emit({ path: this.currentPath, value: this.item.value });
           }
         });
+      }
+    });
+  }
+
+  addNode() {
+    const model: PromptOptions = {
+      message: 'Node key',
+      buttons: [],
+      output: '',
+      validators: [Validators.required]
+    };
+
+    // Fetch the node key
+    this.modalService.prompt(model).then(res => {
+      if (res === ModalResult.OK) {
+        const item = {};
+        item[model.output] = {};
+
+        this.item.value = {
+          ...this.item.value,
+          ...item
+        };
+
+        this.updated.emit({ path: this.currentPath, value: this.item.value });
       }
     });
   }
@@ -132,8 +122,8 @@ export class TranslationItemComponent implements OnChanges {
       title: `Are you sure you want to remove ${this.item.key}?`,
       message,
       buttons: [
-        new ModalButton({result: ModalResult.Cancel, primary: true}),
-        new ModalButton({result: ModalResult.OK}),
+        new ModalButton({ result: ModalResult.Cancel, primary: true }),
+        new ModalButton({ result: ModalResult.OK }),
       ]
     }).then(res => {
       if (res === ModalResult.OK) {
